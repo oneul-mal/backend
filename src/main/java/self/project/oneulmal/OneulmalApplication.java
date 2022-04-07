75893630c4041ad210922163d15d7134a0170411
최창준 <aowert6518@gmail.com>
2022-04-07 16:52:33 +0900

    init: 개발 환경 구축

---
+++ src/main/java/self/project/oneulmal/OneulmalApplication.java
@@ -0,0 +1,13 @@
+package self.project.oneulmal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneulmalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneulmalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/self/project/oneulmal/OneulmalApplicationTests.java
@@ -0,0 +1,13 @@
+package self.project.oneulmal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneulmalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
