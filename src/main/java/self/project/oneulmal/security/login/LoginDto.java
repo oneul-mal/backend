c7b50c939141425f845a0f2d580253ad391c1b58
ChangJun CHOI <58128623+changjun6518@users.noreply.github.com>
2022-05-17 22:16:10 +0900

    Revert "Feat/8"

--- src/main/java/self/project/oneulmal/security/login/LoginDto.java
+++
@@ -1,9 +0,0 @@
-package self.project.oneulmal.security.login;
-
-import lombok.Data;
-
-@Data
-public class LoginDto {
-    private String username;
-    private String password;
-}
--- src/main/java/self/project/oneulmal/user/presentation/dto/UserAssembler.java
+++
@@ -1,9 +0,0 @@
-package self.project.oneulmal.user.presentation.dto;
-
-public class UserAssembler {
-    public static UserRegisterRes userRegisterRes(UserRegisterReq userRegisterReq) {
-        return UserRegisterRes.builder()
-                .name(userRegisterReq.getName())
-                .build();
-    }
-}
